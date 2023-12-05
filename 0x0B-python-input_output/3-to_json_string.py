aa9bfbab156b3d153e57e82614b3bce972f6cebd
Ibenammou <imanebenamou@gmail.com>
2023-12-05 13:16:24 -0800

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" my mdule """
+import json
+
+
+def to_json_string(my_obj):
+    """
+    this function returns the JSON
+    of an object (string)
+    """
+    return json.dumps(my_obj)
