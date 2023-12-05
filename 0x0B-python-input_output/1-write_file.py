61276abd666e5aac005d813bd6713a8539d56e67
Ibenammou <imanebenamou@gmail.com>
2023-12-05 13:07:04 -0800

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,27 +1,9 @@
 #!/usr/bin/python3
-"""
-Module for writing to a file
-"""
+""" my module """
 
 
 def write_file(filename="", text=""):
-    """
-    Writes a string to a text file (UTF8) and returns the number of characters written.
-
-    Args:
-        filename (str): The name of the file.
-        text (str): The text to write to the file.
-
-    Returns:
-        int: The number of characters written.
-    """
-    with open(filename, mode='w', encoding='utf-8') as file:
-        return file.write(text)
-
-
-if __name__ == "__main__":
-    filename = "my_first_file.txt"
-    text = "This School is so cool!\n"
-    nb_characters = write_file(filename, text)
-    print(nb_characters)
-
+    """ Write a file """
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
+        return len(text)
