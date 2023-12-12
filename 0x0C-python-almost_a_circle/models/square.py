91e07a06933414095de55db3f0fa4dd8e4c8d77a
Ibenammou <imanebenamou@gmail.com>
2023-12-11 17:15:20 -0800

    sqaure

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,30 +9,47 @@ class Square(Rectangle):
         """ Constructor method """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """ Getter method for size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Setter method for size """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ Override __str__ method to represent Square object as a string """
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width
         )
 
+    def update(self, *args, **kwargs):
+        """ Update attributes with keyworded arguments """
+        if args:
+            attributes = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attributes[i], args[i])
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Return dictionary representation of Square """
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
+
 if __name__ == "__main__":
     # Example usage
     s1 = Square(5)
     print(s1)
-    print(s1.area())
-    s1.display()
+    print(s1.size)
+    s1.size = 10
+    print(s1)
 
-    print("---")
-
-    s2 = Square(2, 2)
-    print(s2)
-    print(s2.area())
-    s2.display()
-
-    print("---")
-
-    s3 = Square(3, 1, 3)
-    print(s3)
-    print(s3.area())
-    s3.display()
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
