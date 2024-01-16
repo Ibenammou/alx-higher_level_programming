903f0587a5f6f4142b73927a24afdf4dc97255a7
Ibenammou <imanebenamou@gmail.com>
2024-01-16 13:42:31 -0800

    task102

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- script that displays the top 3 cities
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` WHERE `month` >= 7 AND `month` <= 8 GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
