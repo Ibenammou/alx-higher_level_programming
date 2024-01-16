9e67978da355200ba2a1f8e60ac735ab90eb7335
Ibenammou <imanebenamou@gmail.com>
2024-01-16 13:24:02 -0800

    task0

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,7 +1,2 @@
--- 0-list_databases.sql
-
--- This query selects all database names from the information_schema.SCHEMATA table.
--- The result is a list of databases on the MySQL server.
-
-SELECT schema_name
-FROM information_schema.SCHEMATA;
+-- List all databased in MySQL server
+SHOW DATABASES;
