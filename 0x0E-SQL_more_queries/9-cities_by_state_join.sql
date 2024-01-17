ea90b647af680cb3c2bb93e2309290eedf2f9e61
Ibenammou <imanebenamou@gmail.com>
2024-01-17 15:39:06 -0800

    task

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,10 +1,8 @@
--- script to list all cities contained in the database
+-- 9-cities_by_state_join.sql
 
--- Use the database
-USE hbtn_0d_usa;
-
--- Select all cities with their corresponding names using join
-SELECT cities.id, cities.name, states.name
+-- List all cities with their corresponding states
+SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
-LEFT JOIN states ON cities.state_id = states.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
+
