d6417e284388d18440364110c0880caeba83da7a
Ibenammou <imanebenamou@gmail.com>
2024-02-15 13:42:16 -0800

    javascript

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// define the add function
+function add (a, b) {
+  return (a + b);
+}
+
+// export the add function
+module.exports = { add };
---
+++ 0x12-javascript-warm_up/13-main.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// import the add function from the 13-add module
+const add = require('./13-add').add;
+
+// use the add function and print result
+console.log(add(3, 5));
