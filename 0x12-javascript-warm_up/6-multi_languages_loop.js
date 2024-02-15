1756e0ea31d9557a0f6ee114886afdeaf6c53fcf
Ibenammou <imanebenamou@gmail.com>
2024-02-15 13:22:12 -0800

    javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Array of strings
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Loop through the array and use console.log(...) to print
+for (const line of lines) {
+  console.log(line);
+}
