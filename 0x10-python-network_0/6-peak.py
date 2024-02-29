c4c867ce219027ed0451e57629f43803116414ff
Ibenammou <imanebenamou@gmail.com>
2024-02-29 14:13:48 -0800

    task

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Function to find a peak in alist of unsorted
+integers.
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Args:
+        list_of_integers (list): List of unsorted integers
+    Returns:
+        int: A peak in the list, or None if the list is empty
+    """
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] <= list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
