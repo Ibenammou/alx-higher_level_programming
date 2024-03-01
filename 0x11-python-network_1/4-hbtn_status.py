17bcbf90ac129c690b569c78f0ec215a5a8e3769
Ibenammou <imanebenamou@gmail.com>
2024-03-01 15:27:07 -0800

    task

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status using the requests
+package
+"""
+
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    response = requests.get(url)
+
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
