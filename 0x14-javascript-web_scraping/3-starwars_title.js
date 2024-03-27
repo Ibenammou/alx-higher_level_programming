9aeb289e9d98e4c030da01d03e8abfe46bb4f482
root <imanebenamou@gmail.com>
2024-03-27 00:25:01 +0000

    task

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// script to use 'request' module to make a GET request to the
+// Star Wars API endpoint
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  }
+});
